128e9ba3332bc6d9d6884833bc20d1390a90ce02
hasan <hasanismaiel19951995@gmail.com>
2024-01-01 12:29:09 +0200

    here is the update

---
+++ app/Providers/BlogServiceProvider2.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Blog\app\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogServiceProvider2 extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     */
+    public function provides(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('blog::layouts.master')
+
+@section('content')
+    <h1>Hello World from here you canfsd see alot</h1>
+
+    <p>Module: {!! config('blog.name') !!}</p>
+@endsection
